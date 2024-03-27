96a429d483897ace24e29d59cfc2f228184852a6
Abid Ullah <abidullah.se@gmail.com>
2024-03-27 15:46:52 +0500

    scss added

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,8 @@
+import styles from './login.module.scss'
 const Login = () => {
     return (
         <>
-            <h2>Login</h2>
+            <h2 className={styles.heading}>Login</h2>
         </>
     )
 
